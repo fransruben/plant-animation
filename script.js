5087b8b7a43870dd325bb9c707fa894829a3265f
Ruben van den Hout <ruben@MacBook-Pro.local>
2020-03-01 15:11:49 +0100

    Initial version

---
+++ script.js
@@ -0,0 +1,10 @@
+// document.addEventListener("DOMContentLoaded", function(event) { 
+// });
+
+function animatePlant(){
+    // Add event listener to start animation
+    var el = document.getElementById('plant');
+    var toggle = document.getElementById('switch-input');
+
+    el.classList.toggle('grow-plant');
+}
